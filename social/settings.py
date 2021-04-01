369a09b2b45cb27ec12c85b3a1ec2004819cfea6
Nithin-infinity <nithinms715@gmail.com>
2021-04-01 18:17:17 +0530

    Static Root added

--- social/settings.py
+++ social/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'static/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
